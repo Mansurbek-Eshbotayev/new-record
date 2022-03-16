6e39ee8bde8ebaa9bb3072cdb4baca257bb2f97f
Mansurbek-Eshbotayev <mustafoimron1820@gmail.com>
2022-03-17 03:13:54 +0500

    Add all change

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var elSection = document.querySelector(".game");
+var elTitle = document.querySelector(".title");
+var elText = document.querySelector(".text");
 var elResult1 = document.querySelector(".result1");
 var elResult2 = document.querySelector(".result2");
 var elResult3 = document.querySelector(".result3");
@@ -18,21 +20,93 @@ record.lang = "uz-UZ";
 record.onresult = function(evt){
   var commands = evt.results[0][0].transcript;
 
-  if(commands == "birinchi qizil" ){
-    elResult1.textContent = "red";
-    elResult1.style.backgroundColor = ("red");
+  if(commands == "start"){
+    elSection.style.display = "block";
+    elTitle.style.display = "block";
+    elText.style.display = "none"
   }
-   else if (commands == "birinchi yashil" ){
-    elResult1.textContent = "green"
-    elResult1.style.backgroundColor = ("green");
+
+  if(commands == "birinchi sariq"){
+    elResult1.textContent = "X";
+    elResult1.classList.remove("bkgreen")
+    elResult1.classList.add("bkred")
   }
-   else if (commands == "ikkinchi qizil" ){
-    elResult2.textContent = "red"
-    elResult2.style.backgroundColor = ("red");
+   else if (commands == "birinchi yashil"){
+    elResult1.textContent = "O"
+    elResult1.classList.add("bkgreen")
   }
-   else if (commands == "ikkinchi yashil " ){
-    elResult2.textContent = "green"
-    elResult2.style.backgroundColor = ("green");
+   else if (commands == "ikkinchi sariq"){
+    elResult2.textContent = "X";
+    elResult2.classList.remove("bkgreen")
+    elResult2.classList.add("bkred")
+  }
+   else if (commands == "ikkinchi yashil"){
+    elResult2.textContent = "O"
+    elResult2.classList.add("bkgreen")
+  }
+  else if (commands == "uchinchi sariq"){
+   elResult3.textContent = "X";
+   elResult3.classList.remove("bkgreen")
+   elResult3.classList.add("bkred")
+ }
+  else if (commands.includes("uchinchi sariq")){
+   elResult3.textContent = "O"
+   elResult3.classList.add("bkgreen")
+  }
+  else if (commands == "tortinchi sariq"){
+   elResult4.textContent = "X";
+   elResult4.classList.remove("bkgreen")
+   elResult4.classList.add("bkred")
+ }
+  else if (commands == "tortinchi yashil"){
+   elResult4.textContent = "O"
+   elResult4.classList.add("bkgreen")
+  }
+  else if (commands == "beshinchi sariq"){
+   elResult5.textContent = "X";
+   elResult5.classList.remove("bkgreen")
+   elResult5.classList.add("bkred")
+ }
+  else if (commands == "beshinchi yashil"){
+   elResult5.textContent = "O"
+   elResult5.classList.add("bkgreen")
+  }
+  else if (commands.includes("oltinchi sariq")){
+   elResult6.textContent = "X";
+   elResult6.classList.remove("bkgreen")
+   elResult6.classList.add("bkred")
+ }
+  else if (commands.includes("oltinchi yashil")){
+   elResult6.textContent = "O"
+   elResult6.classList.add("bkgreen")
+  }
+  else if (commands.includes("ettinchi sariq")){
+   elResult7.textContent = "X";
+   elResult7.classList.remove("bkgreen")
+   elResult7.classList.add("bkred")
+ }
+  else if (commands.includes("ettinchi yashil")){
+   elResult7.textContent = "O"
+   elResult7.classList.add("bkgreen")
+  }
+  else if (commands.includes("sakkizinchi sariq")){
+   elResult8.textContent = "X";
+   elResult8.classList.remove("bkgreen")
+   elResult8.classList.add("bkred")
+ }
+  else if (commands.includes("sakkizinchi yashil")){
+   elResult8.textContent = "O"
+   elResult8.classList.add("bkgreen")
+  }
+  else if (commands.includes("oxirgi sariq")){
+   elResult9.textContent = "X";
+   elResult9.classList.remove("bkgreen")
+   elResult9.classList.add("bkred")
+ }
+  else if (commands.includes("oxirgi yashil")){
+   elResult9.textContent = "O"
+   elResult9.classList.add("bkgreen")
+
   }else{
     console.log("error")
   }
@@ -48,5 +122,6 @@ record.onresult = function(evt){
 
 
 btnRec.addEventListener("click" , function(){
+  btnRec.classList.add("ops")
   record.start();
 })
